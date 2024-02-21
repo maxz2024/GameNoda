dab3649f987305d6b96ca1cad2e475a152296e36
pa7lux <pa7lux@gmail.com>
2024-02-21 19:26:26 +0400

    first commit

---
+++ AppModules/games/index.js
@@ -0,0 +1,8 @@
+const knightDragonAndPrincessGame = require('./knight-dragon-princess');
+const poleChudesGame = require('./pole-chudes');
+const trueOrFalseGame = require('./true-or-false');
+const dropCoin = require('./coin');
+const makeWordGame = require('./make-word');
+const blackJackGame = require('./black-jack');
+
+module.exports = {knightDragonAndPrincessGame, poleChudesGame, trueOrFalseGame, dropCoin, makeWordGame, blackJackGame};
